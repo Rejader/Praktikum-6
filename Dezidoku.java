defbddd437bd10bdd0054bfb1bb0fb6f1f9fc26c
SteTho7 <47400814+SteTho7@users.noreply.github.com>
2019-02-06 21:53:25 +0100

    Add files via upload

---
+++ Dezidoku.java
@@ -0,0 +1,11 @@
+public class Dezidoku {
+	public enum Variante {normal, mitDiagonalen}
+	private Variante v;
+	// was sonst noch noetig ist
+	
+	public Dezidoku(Variante v) {/*TODO*/}
+	public void read() {/*TODO*/}
+	public void write() {/*TODO*/}
+	public boolean check() {/*TODO*/}
+	public void solve() {/*TODO*/}
+}
